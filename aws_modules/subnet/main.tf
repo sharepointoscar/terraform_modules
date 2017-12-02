e1898ad3a4afe3d11f751fc3af7e03b283390176
Oscar Medina <oscar@sharepointace.com>
2017-12-02 11:50:48 -0800

    Added name attribute
    the subnet needed the name attribute to be set, as the output.tf defines it.

--- aws_modules/subnet/main.tf
+++ aws_modules/subnet/main.tf
@@ -1,4 +1,5 @@
 resource "aws_subnet" "main" {
+  name              = "${var.aws_subnet.main.name}"
   availability_zone = "${var.availability_zone}"
   vpc_id            = "${var.vpc_id}"
   cidr_block        = "${var.cidr_block}"
