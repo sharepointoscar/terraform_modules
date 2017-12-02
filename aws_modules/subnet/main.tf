c823dd86862469d99d46c6ec1d3f8d793c9fa102
Oscar Medina <oscar@sharepointace.com>
2017-12-02 15:33:39 -0800

    added public ip mapping option

--- aws_modules/subnet/main.tf
+++ aws_modules/subnet/main.tf
@@ -2,7 +2,7 @@ resource "aws_subnet" "main" {
   availability_zone = "${var.availability_zone}"
   vpc_id            = "${var.vpc_id}"
   cidr_block        = "${var.cidr_block}"
-
+  map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
   tags {
     Name = "${var.subnet_name}"
   }
