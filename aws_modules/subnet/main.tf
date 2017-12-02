764fd820ce9d0f25ec89385a7be0f6eb8658470c
Oscar Medina <oscar@sharepointace.com>
2017-12-02 11:54:50 -0800

    fixed subnet name

--- aws_modules/subnet/main.tf
+++ aws_modules/subnet/main.tf
@@ -1,5 +1,5 @@
 resource "aws_subnet" "main" {
-  name              = "${var.aws_subnet.main.name}"
+  subnet_name       = "${var.subnet_name}"
   availability_zone = "${var.availability_zone}"
   vpc_id            = "${var.vpc_id}"
   cidr_block        = "${var.cidr_block}"
