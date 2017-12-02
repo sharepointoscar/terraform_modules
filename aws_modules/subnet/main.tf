a51d86d41d827abfbb15507532552fd3accaa9b6
Oscar Medina <oscar@sharepointace.com>
2017-12-02 11:56:19 -0800

    Update main.tf

--- aws_modules/subnet/main.tf
+++ aws_modules/subnet/main.tf
@@ -1,5 +1,4 @@
 resource "aws_subnet" "main" {
-  subnet_name       = "${var.subnet_name}"
   availability_zone = "${var.availability_zone}"
   vpc_id            = "${var.vpc_id}"
   cidr_block        = "${var.cidr_block}"
